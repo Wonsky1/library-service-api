eaf658607c20efd2e5fe130cad9782b28d066c69
petrykivd <petrykiv.dmytro19@gmail.com>
2023-12-13 21:29:24 +0200

    Refactored library_service_api to library_config

--- library_config/urls.py
+++ library_config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library_service_api project.
+URL configuration for library_config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
