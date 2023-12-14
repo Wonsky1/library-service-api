75d99c1f0a655f7a74f1a5896beb65f9d691f9b7
Denys <den.prislipskyi@gmail.com>
2023-12-14 10:44:47 +0100

    Add book model

--- library/models.py
+++ library/models.py
@@ -2,6 +2,18 @@ from django.conf import settings
 from django.db import models
 
 
+class Book(models.Model):
+    COVER_CHOICES = {
+        "H": "HARD",
+        "S": "SOFT"
+    }
+    title = models.CharField(max_length=128)
+    author = models.CharField(max_length=150)
+    cover = models.CharField(max_length=1, choices=COVER_CHOICES)
+    inventory = models.PositiveIntegerField()
+    daily = models.DecimalField()
+
+
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
