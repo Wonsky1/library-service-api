506f142f36f3767ac0e454cab7013599090ba3a6
Denys <den.prislipskyi@gmail.com>
2023-12-14 13:14:23 +0100

    Add detail serializer for Book

--- library/serializers.py
+++ library/serializers.py
@@ -7,3 +7,15 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily")
+
+
+class BookDetailSerializer(BookSerializer):
+    borrowings = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="user.email"
+    )
+
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "inventory", "daily", "borrowings")
