733933c3565864b7a91608ec48b7400683f1128e
petrykivd <petrykiv.dmytro19@gmail.com>
2023-12-15 20:27:11 +0200

    Fixed urls

--- library/urls.py
+++ library/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from library.views import BookViewSet
 
 router = routers.DefaultRouter()
-router.register("book", BookViewSet)
+router.register("", BookViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
