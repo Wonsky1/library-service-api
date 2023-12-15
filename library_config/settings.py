184ed1f6367593bdc6c914e7c76e735f03b3a440
petrykivd <petrykiv.dmytro19@gmail.com>
2023-12-15 18:33:27 +0200

    Created stripe_helper

--- library_config/settings.py
+++ library_config/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
---
+++ payment/stripe_helper.py
@@ -0,0 +1,106 @@
+import stripe
+
+from django.urls import reverse
+
+from library_config import settings
+from payment.models import Payment
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+FINE_MULTIPLIER = 1.5
+
+
+def count_total_price(borrowing):
+
+    if borrowing.borrow_date == borrowing.actual_return_date:
+        days_borrowing = 1
+
+    elif borrowing.expected_return_date >= borrowing.actual_return_date:
+        days_borrowing = (borrowing.actual_return_date
+                          - borrowing.borrow_date
+                          ).days
+    else:
+        days_borrowing = (borrowing.expected_return_date
+                          - borrowing.borrow_date
+                          ).days
+
+    price_in_cents = int(days_borrowing
+                         * borrowing.book.daily
+                         * 100
+                         )
+
+    overdue_days = (borrowing.actual_return_date
+                    - borrowing.expected_return_date
+                    ).days
+
+    fine_in_cents = int(overdue_days
+                        * borrowing.book.daily
+                        * FINE_MULTIPLIER
+                        * 100
+                        ) if overdue_days > 0 else 0
+
+    return price_in_cents + fine_in_cents
+
+
+def create_payment(borrowing, session):
+    payment = Payment.objects.create(
+        status="PENDING",
+        type="PAYMENT",
+        borrowing=borrowing,
+        session_id=session.id,
+        session_url=session.url,
+        user=borrowing.user
+    )
+
+    if borrowing.actual_return_date > borrowing.expected_return_date:
+        payment.payment_type = "FINE"
+
+    payment.money_to_pay = round(
+        count_total_price(borrowing) / 100, 2
+    )
+    payment.save()
+
+    return payment
+
+
+def create_stripe_session(borrowing, request):
+
+    success_url = request.build_absolute_uri(
+        reverse(
+            "payment:payment-success",
+            args=[borrowing.id]
+        )
+    )
+    cancel_url = request.build_absolute_uri(
+        reverse(
+            "payment:payment-cancel",
+            args=[borrowing.id]
+        )
+    )
+
+    total_price_in_cents = count_total_price(borrowing)
+
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[{
+            "price_data": {
+                "currency": "usd",
+                "unit_amount": total_price_in_cents,
+                "product_data": {
+                    "name": borrowing.book.title,
+                    "description": f"User: {borrowing.user.email}",
+                },
+            },
+            "quantity": 1,
+        }],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
+
+    payment = create_payment(borrowing, session)
+
+    borrowing.payments.add(payment)
+    borrowing.save()
+
+    return session.url
