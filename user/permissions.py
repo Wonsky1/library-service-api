c72895fac57dde83636c77aeb1f5ad2542e8d00d
wonsky <pidborskii17@gmail.com>
2023-12-17 11:37:05 +0200

    Blacked user app and manage.py, flake8 update

--- user/permissions.py
+++ user/permissions.py
@@ -3,14 +3,14 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAdminOrReadOnly(BasePermission):
     """
-        Only admin users can create/update/delete books
-        All users (even those not authenticated) able only to read/retrieve
+    Only admin users can create/update/delete books
+    All users (even those not authenticated) able only to read/retrieve
     """
+
     def has_permission(self, request, view):
         return bool(
             (request.user and request.user.is_staff)
-            or
-            request.method in SAFE_METHODS
+            or request.method in SAFE_METHODS
         )
 
 
@@ -27,13 +27,7 @@ class IsAdminOrIfAuthenticatedReadAndCreateOnly(BasePermission):
 
 
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
-
     def has_permission(self, request, view):
-
         return (
-            (
-                request.method in SAFE_METHODS
-                and request.user.is_authenticated
-            )
-            or (request.user.is_authenticated and request.user.is_staff)
-        )
+            request.method in SAFE_METHODS and request.user.is_authenticated
+        ) or (request.user.is_authenticated and request.user.is_staff)
