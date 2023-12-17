47116ec00118d9be48ecafd9c84f4a0572b890c3
wonsky <pidborskii17@gmail.com>
2023-12-17 19:28:05 +0200

    Implemented books borrowing task

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -1,17 +1,17 @@
+import os
+
+import requests
+
 from borrowing.models import Borrowing
 from celery import shared_task
 from django.utils import timezone
-from borrowing.serializers import TaskSerializer
-from notifications.bot_commands import send_overdue_notification
 
 
 @shared_task
 def borrowing_books():
     borrowings = Borrowing.objects.all()
-    users = {}
     for borrowing in borrowings:
         if borrowing.expected_return_date < timezone.localdate() and borrowing.user.telegram_notifications_enabled:
-            borrowing_serializer = TaskSerializer(borrowing)
-            users[borrowing.user.telegram_id] = borrowing_serializer.data
-    # return users
-    send_overdue_notification(users)
+            url = f"https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}/sendMessage?chat_id={borrowing.user.telegram_id}&text=You have an outdated borrowing: {borrowing.book.title}, please return the book"
+            requests.get(url)
+    return "Send borrowing notifications"
