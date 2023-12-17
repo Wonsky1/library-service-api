392edd91f01ac39e9d5712ac999cf3157e7d4f21
Denys <den.prislipskyi@gmail.com>
2023-12-17 10:54:07 +0100

    Fixed tasks

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -2,13 +2,13 @@ from borrowing.models import Borrowing
 from celery import shared_task
 from django.utils import timezone
 
+
 @shared_task
 def count():
     borrowings = Borrowing.objects.all()
     total = 0
-    today = timezone.localdate()
     for borrowing in borrowings:
-        if borrowing.expected_return_date.today() < today:
+        if borrowing.expected_return_date < timezone.localdate():
             total += 1
     return total
 
