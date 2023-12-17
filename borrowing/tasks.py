cfda9760f103a8c07deec8b836acbd19796a9789
Denys <den.prislipskyi@gmail.com>
2023-12-17 10:02:27 +0100

    change task func

--- borrowing/tasks.py
+++ borrowing/tasks.py
@@ -1,13 +1,14 @@
-# Create your tasks here
-
 from borrowing.models import Borrowing
-from user.models import User
-
 from celery import shared_task
-
 from django.utils import timezone
 
 @shared_task
 def count():
-    return User.objects.count()
+    borrowings = Borrowing.objects.all()
+    total = 0
+    today = timezone.localdate()
+    for borrowing in borrowings:
+        if borrowing.expected_return_date.today() < today:
+            total += 1
+    return total
 
